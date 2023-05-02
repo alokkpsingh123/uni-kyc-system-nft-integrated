a0a91f55e84b9fb1d2742076392862a7a811437e
Mangesh <63477318+mangeshkchauhan@users.noreply.github.com>
2023-05-02 14:02:44 +0530

    chore: format add-entity file

--- front-end/src/components/add-entity.tsx
+++ front-end/src/components/add-entity.tsx
@@ -1,7 +1,7 @@
-import { Center, HStack, Text } from "native-base";
-import { IoMdAddCircle } from "react-icons/io";
-import { Pressable } from "react-native";
-import { useNavigate } from "react-router-dom";
+import { Center, HStack, Text } from 'native-base';
+import { IoMdAddCircle } from 'react-icons/io';
+import { Pressable } from 'react-native';
+import { useNavigate } from 'react-router-dom';
 
 export const AddEntity = ({
   entity,
@@ -13,16 +13,17 @@ export const AddEntity = ({
   const navigate = useNavigate();
   return (
     <Pressable onPress={() => navigate(route)}>
-      <HStack bg={"white"} maxWidth={["400px", "300px"]} cursor={"pointer"}>
+      <HStack bg={'white'} maxWidth={['400px', '300px']} cursor={'pointer'}>
         <Center padding={8}>
-          <IoMdAddCircle size={50} color="#facc15" />
+          <IoMdAddCircle size={50} color='#facc15' />
         </Center>
         <Center paddingRight={10}>
           <Text
-            fontSize={"lg"}
-            color="coolGray.400"
-            fontWeight={"semibold"}
-            textTransform={"uppercase"}>
+            fontSize={'lg'}
+            color='coolGray.400'
+            fontWeight={'semibold'}
+            textTransform={'uppercase'}
+          >
             Add
             <br />
             {entity}
